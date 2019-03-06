7d73984aaf9dd753e59a2d379c561e960b94754d
cstorm125 <cebril@gmail.com>
2019-03-06 21:26:18 +0700

    100dayofcode

--- position_bot/qnetwork.py
+++ position_bot/qnetwork.py
@@ -5,16 +5,16 @@ from torch import optim
 from torch.autograd.variable import Variable
 
 class QNetwork(nn.Module):
-    def __init__(self,in_channel=6,nb_hidden=64,action_size=3,seed=1412):
+    def __init__(self, in_channel=6, nb_hidden=64, action_size=3, seed=1412):
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.feature = nn.Sequential(
-            nn.Conv1d(in_channel,12,kernel_size=6, stride=2),
+            nn.Conv1d(in_channel,12,kernel_size=3, stride=1),
             nn.ReLU(),
-            nn.Conv1d(12, 24, kernel_size=6, stride=3),
+            nn.Conv1d(12, 24, kernel_size=3, stride=2),
             nn.ReLU()
         )
-        self.feature_size = 192
+        self.feature_size = 672
         self.head = nn.Sequential(
             nn.Linear(self.feature_size, nb_hidden),
             nn.ReLU(),
